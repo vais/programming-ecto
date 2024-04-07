09573a660d72b875ac049aafeff525fa7194feaa
Vais Salikhov <vsalikhov@gmail.com>
2024-04-06 22:49:41 -0400

    make existing tests pass

--- lib/music_db/album.ex
+++ lib/music_db/album.ex
@@ -1,11 +1,11 @@
-#---
+# ---
 # Excerpted from "Programming Ecto",
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
 # We make no guarantees that this code is fit for any purpose.
 # Visit https://pragprog.com/titles/wmecto for more book information.
-#---
+# ---
 defmodule MusicDB.Album do
   use Ecto.Schema
   alias MusicDB.{Artist, Track, Genre}
@@ -19,4 +19,7 @@ defmodule MusicDB.Album do
     many_to_many(:genres, Genre, join_through: "albums_genres")
   end
 
+  def changeset(%__MODULE__{} = album, params \\ %{}) do
+    Ecto.Changeset.cast(album, params, [:title])
+  end
 end
--- test/music_db/album_test.exs
+++ test/music_db/album_test.exs
@@ -1,11 +1,11 @@
-#---
+# ---
 # Excerpted from "Programming Ecto",
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
 # We make no guarantees that this code is fit for any purpose.
 # Visit https://pragprog.com/titles/wmecto for more book information.
-#---
+# ---
 defmodule MusicDB.AlbumTest do
   use ExUnit.Case, async: true
   alias MusicDB.{Repo, Album}
@@ -33,5 +33,4 @@ defmodule MusicDB.AlbumTest do
     album = Ecto.Changeset.apply_changes(changeset)
     assert album.title == "Dark Side of the Moon"
   end
-
 end
