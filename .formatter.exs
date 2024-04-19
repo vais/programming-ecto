76127c1cfc3ec203d79e36b8c9872c3306b5ee23
Vais Salikhov <vsalikhov@gmail.com>
2024-04-18 22:32:18 -0400

    fix the formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,12 +1,13 @@
-#---
+# ---
 # Excerpted from "Programming Ecto",
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
 # We make no guarantees that this code is fit for any purpose.
 # Visit https://pragprog.com/titles/wmecto for more book information.
-#---
+# ---
 # Used by "mix format"
 [
+  import_deps: [:ecto],
   inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
